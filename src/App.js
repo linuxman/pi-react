07238b0d12790e1f2a69308b66ab5984c6d011f6
LinuxmanR4 <inguanzo@gmail.com>
2024-04-25 16:27:19 -0600

    Agregué el favicon

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Nilakantha from "./components/Nilakantha.js";
 const App = () => {
   return (
     <div>
-      <h1>Pi con React.</h1>
+      <h1>Calculando valores de Pi con React.</h1>
       <hr></hr>
       <Leibniz />
       <hr></hr>
