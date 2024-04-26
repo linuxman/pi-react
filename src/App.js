5a94cf84ffb348572b4c3af6dd979be1e5a81bcf
LinuxmanR4 <inguanzo@gmail.com>
2024-04-26 09:57:40 -0600

    Se agregó un texto en la parte superior

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ const App = () => {
   return (
     <div>
       <h1>Calculando valores de &pi; con React.</h1>
+      <p>
+        Aquí hay dos ejemplos de métodos de series infinitas para hacer una
+        aproximación al valor de &pi;.
+      </p>
+      <p>
+        La precisión de la aproximación depende del número de términos de la
+        serie que se sumen. Generalmente entre más iteraciones mejor, aunque hay
+        métodos que se aproximan mejor con un menor número de iteraciones.
+      </p>
       <hr></hr>
       <Leibniz />
       <hr></hr>
